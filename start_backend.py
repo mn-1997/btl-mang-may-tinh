3d91c01e582cf47d792643016977b9578193e284
My Nguyen <my.nguyen1997@hcmut.edu.vn>
2026-05-12 15:29:15 +0700

    update start_backend

--- start_backend.py
+++ start_backend.py
@@ -16,10 +16,7 @@
 """
 start_backend
 ~~~~~~~~~~~~~~~~~
-
-This module provides a simple entry point for deploying backend server process
-using the socket framework. It parses command-line arguments to configure the
-server's IP address and port, and then launches the backend server.
+Launch the backend server.
 """
 
 import socket
@@ -27,20 +24,12 @@ import argparse
 
 from daemon import create_backend
 
-# Default port number used if none is specified via command-line arguments.
+# Default port to use if not specified
 PORT = 9000 
 
 if __name__ == "__main__":
-    """
-    Entry point for launching the backend server.
+    # Parse arguments and start the backend server
 
-    This block parses command-line arguments to determine the server's IP address
-    and port. It then calls `create_backend(ip, port)` to start the RESTful
-    application server.
-
-    :arg --server-ip (str): IP address to bind the server (default: 127.0.0.1).
-    :arg --server-port (int): Port number to bind the server (default: 9000).
-    """
 
     parser = argparse.ArgumentParser(
         prog='Backend',
